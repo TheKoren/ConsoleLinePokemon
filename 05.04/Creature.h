d973875516823899b4f03c7a65e77d98d3398c30
TheKoren <44529612+TheKoren@users.noreply.github.com>
2019-05-05 13:35:24 +0200

    Add files via upload

---
+++ 05.04/Creature.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <time.h>
+
+using namespace std;
+
+enum Type { Water, Fire, Grass, Neutral };  //Neutral csak ability lehet
+
+
+/*class Base{
+private:
+double hp;
+string name;
+double basedmg;
+const int maxhp;
+public:
+Base()
+~Base()
+---
+TODO: Ez, hogy működjön a heterogén kollekció?
+}*/
+
+class Creature {
+private:
+	double hp;
+	string name;
+	double basedmg;
+	const int maxhp;
+public:
+	Creature();
+	Creature(double hp, string name, double basedmg, int maxhp); // Konstruktor && Destuktor
+	~Creature();
+	Creature(const Creature & other);
+
+	string getName()const; // Getterek
+	double getHp()const;
+	double getBaseDmg() const;
+	int getMaxHp()const;
+
+	virtual void Print(); //Mindenféle diagnosztika, lekérdezõ, harc
+	virtual bool isAlive();
+	virtual void setHp(double newhp);
+	virtual double attack();; // Ez valami sima crit chances legyen
+};
